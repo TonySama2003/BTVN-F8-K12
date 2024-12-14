815efada3d50edf57873fcd6b32f3fc89cc119b2
Nguyen Truong Thong <125285601+TonySama2003@users.noreply.github.com>
2024-12-14 11:46:54 +0700

    Update ex01.js

--- Day20/ex01.js
+++ Day20/ex01.js
@@ -7,36 +7,36 @@ const students = [
     { name: "Hoang Thi F", score: 6 },
 ];
 
-const result = {
+const studentStats = {
+    highest: students[0],
+    lowest: students[0],
     group: {
         A: [],
         B: [],
         C: [],
         D: [],
     },
-    highest: students[0],
-    lowest: students[0],
 };
 
 for (const student of students) {
     // Kiểm tra sinh viên có điểm cao nhất và thấp nhất
-    if (result.lowest.score > student.score) result.lowest = student;
-    if (result.highest.score < student.score) result.highest = student;
+    if (studentStats.lowest.score > student.score) studentStats.lowest = student;
+    if (studentStats.highest.score < student.score) studentStats.highest = student;
 
     // Sử dụng switch để phân loại nhóm điểm
     switch (true) {
         case student.score < 4:
-            result.group.D.push(student);
+            studentStats.group.D.push(student);
             break;
         case student.score < 6:
-            result.group.C.push(student);
+            studentStats.group.C.push(student);
             break;
         case student.score < 8:
-            result.group.B.push(student);
+            studentStats.group.B.push(student);
             break;
         default:
-            result.group.A.push(student);
+            studentStats.group.A.push(student);
     }
 }
 
-console.log(result);
+console.log(studentStats);
